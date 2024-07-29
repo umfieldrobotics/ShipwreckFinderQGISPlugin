e633eded97ef2574d0f3e004cc58b5f8d9cbce23
drew <drewskis@umich.edu>
2024-07-29 11:34:05 -0400

    Rearanged class structure

--- qgisplugin/interfaces/preprocessing_gui.py
+++ qgisplugin/interfaces/preprocessing_gui.py
@@ -34,89 +34,18 @@ from qgis.PyQt.QtWidgets import QDialog, QFileDialog, QDialogButtonBox
 from qgis.PyQt.QtCore import Qt
 
 from qgis.PyQt.QtWidgets import (
-    QMenu,
-    QAction,
     QDialog,
-    QVBoxLayout,
     QDialogButtonBox,
-    QLabel,
-    QApplication,
-    QPushButton
 )
-from qgis.PyQt.QtCore import QCoreApplication, pyqtSignal
-from qgis.PyQt.QtGui import QCursor
 from qgis.PyQt.QtGui import QPixmap
 
-from qgisplugin.core.ship_seeker import ShipSeeker
-from qgisplugin.interfaces import import_image, write_image
-from qgisplugin.interfaces.RectangleMapTool import RectangleMapTool
+from qgisplugin.core.preprocessing_handler import PreprocessingHandler
 from qgisplugin.core.tiff_utils import copy_tiff_metadata
 
 
 import matplotlib.pyplot as plt
 import cv2
 
-
-
-
-class PreprocessingHandler:
-    def __init__(self): 
-        pass
-
-    def normalize(self, depth_array, set_progress: callable = None, 
-                  kernel_size=200, inpaint_radius=10):
-        depth_array[depth_array > 10000] = np.nan
-
-
-        nan_mask = np.isnan(depth_array).astype(np.float32)
-        depth_array_with_zeros = np.where(nan_mask, 0, depth_array).astype(np.float32)
-
-        # Kernel
-        kernel = np.ones((kernel_size, kernel_size), dtype=np.float32)
-
-        matrix_sum = cv2.filter2D(depth_array_with_zeros, -1, kernel)
-        not_nan_count = cv2.filter2D(1-nan_mask, -1, kernel) 
-
-        set_progress(30)
-
-        local_mean_img = matrix_sum / np.maximum(not_nan_count, 1.0)
-
-        local_mean_img[nan_mask == 1] = 0 # TODO: is this right?
-
-
-
-        # Sbtraced image (x_i - u)
-        subtracted_img = np.abs(depth_array_with_zeros - local_mean_img)
-
-        # Compute the std_dev
-        subtracted_img_sqr = subtracted_img ** 2
-
-        subtracted_img_sqr_summed = cv2.filter2D(subtracted_img_sqr, -1, kernel)
-        subtracted_img_sqr_summed[subtracted_img_sqr_summed < 0] = 0
-        set_progress(40)
-
-        local_variance_img = subtracted_img_sqr_summed / np.maximum(not_nan_count, 1.0)
-
-        local_stddev_img = np.sqrt(local_variance_img)
-
-        local_stddev_img[local_stddev_img == 0] = 1 # make sure re we're not dividing by 0
-
-
-        # DO a cutoff instead
-        # subtracted_img[np.abs(subtracted_img)>local_stddev_img] *= 10
-
-        normalized_raw = subtracted_img / np.sqrt(local_stddev_img)
-
-        set_progress(50)
-
-        normalized_img = cv2.normalize(normalized_raw.astype(np.float32), None, .5, 1.0, cv2.NORM_MINMAX)
-        normalized_img[nan_mask == 1] = 0
-        inpainted = cv2.inpaint((normalized_img * 255).astype(np.uint8), nan_mask.astype(np.uint8), inpaint_radius, cv2.INPAINT_TELEA)
-        rgb_grayscale_image = cv2.cvtColor((inpainted).astype(np.uint8), cv2.COLOR_GRAY2RGB)
-
-        set_progress(80)
-
-        return rgb_grayscale_image
 
 
 class PreprocessingWidget(QDialog):
@@ -241,44 +170,6 @@ class PreprocessingWidget(QDialog):
                 QgsProject.instance().addMapLayer(output_raster_layer, True)
 
         self.progressBar.setValue(100)
-        
-
-        return
-        try:
-            # Only temp file possible when result is opened in QGIS
-            output_path = self.outputFileWidget.filePath()
-
-            if not self.openCheckBox.isChecked() and len(output_path) == 0:
-                raise Exception("If you won't open the result in QGIS, you must select a base file name for output.")
-
-            # Get parameters
-            raster_layer = self.imageDropDown.currentLayer()
-
-            image_path = self.imageDropDown.currentLayer().source()
-            image, metadata = import_image(image_path)
-
-            extent_str = self.extentText.text()
-            print("Just got the text...")
-
-            # run code
-            result = ShipSeeker(raster_layer=raster_layer, extent_str=extent_str)\
-                .execute(output_path, set_progress=self.progressBar.setValue, log=self.log)
-
-            self.progressBar.setValue(100)
-
-            # write image to file
-            print("THe output path will be: ", output_path)
-
-            # Open result in QGIS
-            if self.openCheckBox.isChecked():
-                output_raster_layer = QgsRasterLayer(output_path, 'New Image')
-                QgsProject.instance().addMapLayer(output_raster_layer, True)
-
-        # except AttributeError:
-        #     self.log("Please select an image.")
-        except Exception as e:
-            self.log(e)
-            raise e
 
 
 def _run():
